f4da450178fc2c425a6a5127607f9561e4977da0
NakanoSanku <AlexKaykt@outlook.com>
2023-02-28 14:49:17 +0800

    新增活动脚本

--- src/yys/活动.js
+++ src/yys/活动.js
@@ -1,26 +1,57 @@
 const { findImg } = require("../base/search");
-const readJson = require("../readJson");
+const debug = require("../debug");
 const settlement = require("./settlement");
 const 协作检测 = require("./协作检测");
 /**
- * 
+ *
  * @param {{
- * configPath:
- * method
- * settlementArray}} params 
+ * times:次数
+ * speed:是否启用光速模式
+ * screenshotLoad:截图根路径
+ * settlementArray:自定义结算}} params
  */
 module.exports = function (params) {
-    let configPath = params.configPath || './src/config/活动.json';
-    let config = readJson(configPath);
-    config.settlement.method = params.method || config.settlement.method;
-    if (config.settlement.method == 2) config.settlement.settlementArray = params.settlementArray || config.settlement.settlementArray;
-    let i = 0;
-    while (i < config.times) {
-        if (findImg(config.challenge)) sleep(2000);
-        if (settlement(config.settlement)) {
-            i++;
-            console.log('当前第' + i + '次结算');
-        }
-        协作检测();
-    }
-} 
+  let delayTime = params.delayTime || 800;
+  let screenshotLoad = params.screenshotLoad || "./assets/img/";
+  let challenge = {
+    template: screenshotLoad + "活动/活动.bmp",
+    isClick: true,
+    region: [800, 400, 1279, 719],
+  };
+  let settlementArray = params.settlementArray || [
+    [10, 120, 250, 550],
+    [1100, 50, 1280, 720],
+  ];
+  let method = params.settlementArray != undefined ? 2 : 1;
+  let settlement1 = {
+    imgConfig: {
+      template: screenshotLoad + "公用/结算一.bmp",
+      region: [330, 79, 645, 351],
+    },
+    method: method,
+    settlementArray: settlementArray,
+  };
+  let settlement2 = {
+    imgConfig: {
+      template: screenshotLoad + "公用/结算二.bmp",
+      region: [442, 358, 843, 684],
+    },
+    method: method,
+    settlementArray: settlementArray,
+  };
+  let times = params.times || 0;
+  let i = 0;
+  let start = Date.now();
+  while (i < times) {
+    debug({ start });
+    findImg(challenge);
+    settlement(settlement2); //结算二
+    if (settlement(settlement1)) {
+      i++;
+      start = Date.now();
+      console.log("当前第" + i + "次结算");
+    } //结算一
+    if (params.speed) sleep(delayTime);
+    协作检测();
+  }
+};
